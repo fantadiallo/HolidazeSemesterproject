b0f90a544451f500501b8285fdf181b4f139882f
fantadiallo <fatmbo0003@outlook.com>
2025-04-29 13:50:49 +0200

    Set up routing with React Router, cleaned up default CSS, and configured SCSS as main styling entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Pages
+import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
+import VenuePage from './pages/VenuePage/VenuePage';
+import BookingsPage from './pages/BookingsPage/BookingsPage';
+import CreateVenuePage from './pages/CreateVenuePage/CreateVenuePage';
+import EditVenuePage from './pages/EditVenuePage/EditVenuePage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/venue/:id" element={<VenuePage />} />
+        <Route path="/bookings" element={<BookingsPage />} />
+        <Route path="/venues/create" element={<CreateVenuePage />} />
+        <Route path="/venues/edit/:id" element={<EditVenuePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
