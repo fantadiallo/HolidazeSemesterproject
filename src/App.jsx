c8515b632f8fa6d482af82666548c2b0457a4243
fantadiallo <fatmbo0003@outlook.com>
2025-04-30 12:31:30 +0200

    Set up React Router with base routes and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Layout from './components/Layout/Layout';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
@@ -13,17 +14,21 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/venue/:id" element={<VenuePage />} />
-        <Route path="/bookings" element={<BookingsPage />} />
-        <Route path="/venues/create" element={<CreateVenuePage />} />
-        <Route path="/venues/edit/:id" element={<EditVenuePage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="venue/:id" element={<VenuePage />} />
+          <Route path="bookings" element={<BookingsPage />} />
+          <Route path="venues/create" element={<CreateVenuePage />} />
+          <Route path="venues/edit/:id" element={<EditVenuePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/main.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import './styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
