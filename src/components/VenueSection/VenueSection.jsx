24f9230227d61c8a5b2d65ccd192cd4f0cbb998c
fantadiallo <fatmbo0003@outlook.com>
2025-05-07 14:01:41 +0200

    Add Venue section with search and amenity filters

---
+++ src/components/VenueSection/VenueSection.jsx
@@ -0,0 +1,5 @@
+
+import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import Hero from "../../components/herohompage/hero";
+import styles from './VenueSection.module.scss';
