9f4b4a08c4a1e890132f148921d4c38474a72cba
fantadiallo <fatmbo0003@outlook.com>
2025-05-07 15:14:29 +0200

    feat: integrate VenueSection in HomePage layout

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,13 @@
+
 import Hero from "../../components/herohompage/hero";
+import VenueSection from "../../components/VenueSection/VenueSection";
 
 export default function HomePage() {
   return (
     <div>
       <Hero />
       {/* other homepage content */}
+      <VenueSection />
     </div>
   );
 }
