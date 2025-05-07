d40ed074941c46dd5cf7a2655757744d453f5887
fantadiallo <fatmbo0003@outlook.com>
2025-05-07 14:02:19 +0200

    Configure SCSS, Bootstrap, and adjust Vite setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/main.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import './styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
